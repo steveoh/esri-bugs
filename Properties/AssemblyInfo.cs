83b1205adc9e8387e7ae57d6c5167b7a0f61d869
steveoh <sgourley@utah.gov>
2016-11-21 18:17:10 -0700

    :sparkles: #01854519

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.SOESupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CreateGeometryFromWkbVariant")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CreateGeometryFromWkbVariant")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fc7fffc0-cde8-4d11-ba75-698abbcd6468")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AddInPackage("CreateGeometryFromWkbVariant", "48fb44c0-c02e-405c-8a46-afa866c047d2",
+    Author = "agrc-arcgis",
+    Company = "Insert Company Name Here",
+    Date = "11/21/2016 2:33:03 PM",
+    Description = "Insert SOE package description here",
+    TargetProduct = "Server",
+    TargetVersion = "10.3",
+    Version = "1.0")]
